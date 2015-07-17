0e1678e8d4fe00df1892a7bf684e7e2b426df9e8
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2015-07-17 12:30:54 -0500

    Improved styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def customers_icon
+    content_tag("i", "", class: "fa fa-users")
+  end
+
+  def orders_icon
+    content_tag("i", "", class: "fa fa-truck")
+  end
+
+  def menu_items_icon
+    content_tag("i", "", class: "fa fa-book")
+  end
 end
