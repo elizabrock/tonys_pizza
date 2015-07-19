de055a533c9074778f1b580f3614a51f84d966e5
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2015-07-19 14:21:58 -0400

    Add order total to order page

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -24,4 +24,18 @@ class OrderTest < ActiveSupport::TestCase
     refute order.valid?
     assert_equal order.errors[:location], ["can't be blank"]
   end
+
+  test "#total when there are no line items" do
+    order = Order.new
+    assert_equal 0, order.total
+  end
+
+  test "#total combines order_item totals" do
+    menu_item1 = MenuItem.new(price: 3.50)
+    menu_item2 = MenuItem.new(price: 1.00)
+    order = Order.new
+    order.order_items.build(quantity: 1, menu_item: menu_item1)
+    order.order_items.build(quantity: 3, menu_item: menu_item2)
+    assert_equal 6.50, order.total
+  end
 end
